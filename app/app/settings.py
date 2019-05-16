13cd426d46c015e285591028b46ad747cfeabb8c
Iago Neres <iagoneresb@gmail.com>
2019-05-16 09:42:57 -0300

    master | Adicionar waiting for db

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,10 +77,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.env.get('DB_HOST'),
-        'NAME': os.env.get('DB_NAME'),
-        'USER': os.env.get('DB_USER'),
-        'PASSWORD': os.env.get('DB_PASS')
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        # 'PORT': 5432,
     }
 }
 
