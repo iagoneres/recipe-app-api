4b71cac2a169176a40a9ba51916aa90c20ea2ac0
Iago Neres <iagoneresb@gmail.com>
2019-05-19 19:37:03 -0300

    master | Config postgre DB

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -16,7 +16,7 @@ class CommandTests(TestCase):
     @patch('time.sleep', return_value=True)
     def test_wait_for_db(self, ts):
         """Test waiting for db"""
-        with patch('django.db.utils.ConnectionHanddler.__getitem__') as gi:
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 6)
